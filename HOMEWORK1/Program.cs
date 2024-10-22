275327a5117b59d90e49df704f0b04bf778447ce
CodeOtakuu <notbukaaa777@gmail.com>
2024-10-22 16:22:31 +0400

    Добавьте файлы проекта.

---
+++ HOMEWORK1/Program.cs
@@ -0,0 +1,75 @@
+﻿//#1
+
+DateTime CurrentTime = DateTime.Now;
+
+Console.WriteLine("Hima jamy [{0}]e", CurrentTime.Hour);
+
+//#2
+
+//1-in dzevy
+
+for (int i = 100; i > 0; i--)
+{
+    Console.Write("{0} ", i);
+}
+
+Console.WriteLine();
+
+//2-rd dzevy
+
+int Num = 100;
+
+for (int i = 0; i < 100; i++)
+{
+    Console.Write("{0} ", Num);
+    Num--;
+}
+
+Console.WriteLine();
+
+//3-rd dzevy
+
+Num = 100;
+
+while(Num > 0)
+{
+    Console.Write("{0} ", Num);
+    Num--;
+}
+
+Console.WriteLine();
+
+//#3
+
+string UserName = "";
+int UserAge = 0;
+bool log = false;
+
+
+Console.WriteLine("------------------------------");
+Console.WriteLine("Cragric Ogtveluc Araj Grancveq");
+Console.WriteLine();
+Console.WriteLine("         [0] exit");
+Console.WriteLine();
+
+do
+{
+    Console.Write("User Name: ");
+    UserName = Console.ReadLine();
+    Console.Write("Age: ");
+    UserAge = Int32.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    if (UserAge >= 18)
+    {
+        Console.WriteLine($"Bari galust {UserName}, duq nsheciq vor dzer tariqy {UserAge}e");
+        log = true;
+    }
+    else
+    {
+        Console.WriteLine($"{UserName}, Dzer tariqy chi hamapatasxanum");
+        Console.WriteLine();
+    }
+}
+while (!log);
+
